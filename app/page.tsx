be1d35ec256a0ff634ed6d7bd303a7f60f2d413d
jiyeon <jiyeonyun43@gmail.com>
2024-03-21 23:44:32 +0900

    [feat] home페이지 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,24 @@
-import styles from "./page.module.css";
+import Image from "next/image";
+import styles from "./page.module.scss";
+import Board from "./ui/common/\bboard";
 export default function Home() {
-    return <main className={styles.main}>ddd</main>;
+    return (
+        <main className={styles.main}>
+            <div className={styles.titleWrap}>
+                <h1>윤지연</h1>
+                <br />
+                <h2>Web Frontend Developer</h2>
+            </div>
+            <div className={styles.imageBox}>
+                <Image src="/image/rupy.jpg" fill alt={""} />
+            </div>
+            <div className={styles.textContentwrap}>
+                <span>🐹</span> 안녕하세요. 웹 프론트엔드 개발자 윤지연입니다.
+                <br />
+                <br />
+                머시기머시기를 좋아하며 성장하는 습관을 만들어가고 있습니다.
+            </div>
+            <Board />
+        </main>
+    );
 }
---
+++ app/ui/common/board.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import style from "./board.module.scss";
+const Board = () => {
+    return <div className={style.boardWrap}></div>;
+};
+
+export default Board;
